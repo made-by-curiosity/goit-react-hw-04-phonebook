b88742a556fc2eb8b66dc30ef54b960475828d7f
TurtlePower <agnkvdm@gmail.com>
2023-06-22 21:55:06 +0300

    add react hook form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,48 +5,38 @@ import { useForm } from 'react-hook-form';
 // import * as yup from 'yup';
 import { AddButton, PhonebookForm, ErrorText } from './Form.styled';
 
-// const phoneRegExp =
-//   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}?$/;
-
-// const nameRegExp = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
-
-// const schema = yup.object().shape({
-//   name: yup
-//     .string()
-//     .matches(nameRegExp, {
-//       message: 'Разве это похоже на имя?',
-//       excludeEmptyString: false,
-//     })
-//     .required('Ну и как ты поймёшь без имени кому звонишь?'),
-//   number: yup
-//     .string()
-//     .matches(phoneRegExp, {
-//       message: 'Наведи на поле ввода номера, посмотри, что можно писать тут.',
-//       excludeEmptyString: false,
-//     })
-//     .required('А звонить в рельсу будем?'),
-// });
-
-const initialValues = {
-  defaultValues: {
-    name: '',
-    number: '',
-  },
-};
-
 export const AddForm = ({ onSubmit }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm(initialValues);
+    reset,
+  } = useForm({
+    defaultValues: {
+      name: '',
+      number: '',
+    },
+    mode: 'onChange',
+  });
 
   return (
-    <PhonebookForm onSubmit={handleSubmit(onSubmit)}>
+    <PhonebookForm
+      onSubmit={handleSubmit(data => {
+        onSubmit(data);
+        reset();
+      })}
+    >
       <label htmlFor="name">
         <p>Name</p>
         <input
-          {...register('name', { required: 'Name is required' })}
+          {...register('name', {
+            required: 'Name is required',
+            pattern: {
+              value:
+                /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+              message: 'Name must contain only letters',
+            },
+          })}
           type="text"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         />
@@ -56,9 +46,16 @@ export const AddForm = ({ onSubmit }) => {
       <label htmlFor="number">
         <p>Number</p>
         <input
-          {...register('number', { required: 'Number is required' })}
+          {...register('number', {
+            required: 'Number is required',
+            pattern: {
+              value:
+                /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}?$/,
+              message: 'Invalid phone number',
+            },
+          })}
           type="tel"
-          title="Номер телефона должен состоять из цифр и может включать в себя пробелы, прочерки, скобки, и может начинаться с +"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         />
       </label>
       <ErrorText>{errors?.number?.message}</ErrorText>
